60ac01ddfd985c0fc20d1b336f35b7ae2b634f01
Sushant <skp000002@gmail.com>
2021-12-24 13:04:52 +0530

    Code refactor in PostCard component

--- client/src/components/Profile/PostCard.vue
+++ client/src/components/Profile/PostCard.vue
@@ -1,28 +1,4 @@
 <template>
-  <!-- <div class="card mb-5">
-    <div class="card-image">
-      <figure class="image is-4by3">
-        <img :src="post.imageURL" alt="Placeholder image" class="postImage"/>
-      </figure>
-    </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-          </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">{{ post.title }}</p>
-          <p class="subtitle is-6"><time datetime="2016-1-1" class="is-size-7">{{format_date(post.createdAt)}}</time></p>
-        </div>
-      </div>
-
-      <div class="content">
-        {{post.description}}
-      </div>
-    </div>
-  </div> -->
   <div class="postCard mb-6">
     <img class="postImage" :src="post.imageURL" alt="Post" />
     <div class="postDetails">
